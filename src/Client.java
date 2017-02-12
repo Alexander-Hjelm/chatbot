7b6fefa62adcabb5b9afa28bb4e98d9df50642bd
Alexander Hjelm <nick.nertova@gmail.com>
2017-02-12 14:28:08 +0100

    Remove test comments

--- src/Client.java
+++ src/Client.java
@@ -7,15 +7,12 @@ public class Client {
 
 	private int destinationPort;
 	private ChatUI userInter;
-	// åh så bög!
-	// FUL KOMMENTAR
-	// en fin klänning
-	
+
 	public Client(int portIn, ChatUI ui) {
 		destinationPort = portIn;
 		userInter = ui;
 		
-	} 
+	}
 	
 	public void connect(String Adress, int port) throws UnknownHostException, IOException {
 		Socket s = new Socket(Adress, port);
