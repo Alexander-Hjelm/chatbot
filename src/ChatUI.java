261a8ca4af3a25952aca10cb4620570f48f43ea6
Viktor <viktoralatalo@gmail.com>
2017-02-12 15:55:36 +0100

    buttonAction method, start gridbaglayout

--- src/ChatUI.java
+++ src/ChatUI.java
@@ -1,3 +1,8 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -40,21 +45,58 @@ public class ChatUI extends JFrame{
 		
 		
 		
-	
-		
-		
-		
-		
-		
 		panel = new JPanel();
-		
-		
-		
-		
+		buttonAction();
 		createAndShowGUI(panel);
 	}
 	
+	private void buttonAction() {
+		sendButton = new JButton("send");
+		exitButton = new JButton("exit");;
+		sendFileButton = new JButton("send file");
+		newChatButton = new JButton("new chat");
+		
+		
+		sendButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	            // add listener stuff, some get data from myMessagePane for this button.
+	         }
+	      });
+		
+		exitButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	            System.exit(0);
+	         }
+	      });
+		
+		sendFileButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	            // open sendFileUI, open fileChooser etc. 
+	         }
+	      });
+		
+		newChatButton.addActionListener(new ActionListener() {
+	         public void actionPerformed(ActionEvent e) {
+	            // add listener stuff, some get data from myMessagePane for this button.
+	         }
+	      });
+		
+	}
+
 	private void createAndShowGUI(JPanel panel) {
+		
+		messageArea = new JTextArea();
+		titleLabel = new JLabel("MSN 2017"); 
+		otherNamesLabel  = new JLabel();
+		myMessagePane = new JTextPane();
+
+		
+		
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints(); 
+		
+		
+		
 	    //Create and set up the window.
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	  
