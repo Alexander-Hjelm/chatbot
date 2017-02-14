f7626f8d4f11fad9aeca488ad0c8f3ebe03749c8
alhjelm@kth.se <nick.nertova@gmail.com>
2017-02-14 17:59:36 +0100

    send() method in Server now sends the data, not run()
    Also moved server.accept() to startServer() because some guy with a
    tutorial had done that. Renamed socket -> server, ssClient -> socket

--- src/Server.java
+++ src/Server.java
@@ -7,47 +7,43 @@ import java.net.Socket;
 public class Server extends CommunicationsHandler {
 
 	private int port;
-	private ServerSocket socket; 
+	private ServerSocket server;
+	private Socket socket;
 	private DataOutputStream streamOut;
 
 public Server(int portIn) throws IOException {
 	port = portIn;
-	
 	startServer();
-	
 } 	
 	
 	public void startServer() throws IOException {
-		this.socket = new ServerSocket((int) port);
+		this.server = new ServerSocket((int) port);
+		//Wait for an incoming connection
+		socket = server.accept();
 		startThread();
 	}
 	
 	public void stopServer() throws IOException {
-		socket.close();
+		server.close();
 	}
 	
 	@Override
 	public void run() {
 		while (true) {
-			try {
-			Socket ssClient = socket.accept();
-			if (ssClient.isConnected()) {
-				System.out.println("hej");
-			}
-			
-			streamOut = new DataOutputStream(ssClient.getOutputStream());
-			streamOut.writeUTF("AEF=)");
-			streamOut.flush();
-			
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
+			//Listen for messages from client
 		}
 	}	
 	
 	@Override
 	public void send(String msg) {
-		
+		try {
+			
+			
+			streamOut = new DataOutputStream(socket.getOutputStream());
+			streamOut.writeUTF("AEF=)");
+			streamOut.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
