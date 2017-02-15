c3bd8864127609698f535b7bd8a6e311d44c7438
Viktor <viktoralatalo@gmail.com>
2017-02-15 18:49:37 +0100

    Added MyData

---
+++ src/MyData.java
@@ -0,0 +1,16 @@
+
+public class MyData {
+	protected String userName;
+	protected String address;
+	protected boolean aes;
+	protected String key;
+	
+	public MyData(String userNameIn, String addressIn, String keyIn, boolean aesIn) {
+		userName = userNameIn;
+		address = addressIn;
+		key = keyIn;
+		aes = aesIn;
+	}
+	
+}
+
