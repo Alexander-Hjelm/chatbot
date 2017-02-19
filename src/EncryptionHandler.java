172b0f2c0d77e01d3a08fdd9ce8f6325381c7672
Viktor <viktoralatalo@gmail.com>
2017-02-19 15:15:42 +0100

    Added working CaesarCipher

--- src/EncryptionHandler.java
+++ src/EncryptionHandler.java
@@ -1,8 +1,11 @@
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 public class EncryptionHandler {
 	
 	private String thisKey = null;
 	private int thisShift;
+	private int byteRange = 256;
 	
 	public EncryptionHandler(boolean aes){
 		if(aes){
@@ -16,8 +19,8 @@ public class EncryptionHandler {
 	}
 	
 	private int generateCaesarKey() {
-		// TODO Auto-generated method stub
-		return 0;
+		int key = (int) Math.random() * 10000;
+		return key;
 	}
 
 	//dummy
@@ -25,22 +28,54 @@ public class EncryptionHandler {
 		return "";
 		
 	}
-//	
-//	public String encrypt(String message) {
-//		if(!thisKey.equals(null)) {
-//			return "";
-//		}
-//		else {
-//			
-//		}
-//	}
-//	
+	
+	
+	public String getHexFromByte(byte[] b){
+	    return String.format("%x", new BigInteger(1, b));
+	}
+	
+	public String getStringFromHex(String hexString){
+		byte[] b = new BigInteger(hexString,16).toByteArray();
+		String retStr = new String(b, StandardCharsets.UTF_8);	
+		return retStr;
+	}
+	
+	private String encryptCaesar(String inputString) {
+//		
+		byte[] b = inputString.getBytes(StandardCharsets.UTF_8);	
+		
+		for (int i = 0; i < b.length; i++) {
+			
+
+			int current = b[i] & 0xff;
+			System.out.println("currentint " + current);
+			int shifted = ((current + thisShift) % byteRange);
+			b[i] = (byte) shifted;
+		}
+		
+		String outputString = getHexFromByte(b);
+		
+		return(outputString);
+	}
+	
+	private String decryptCaesar(String hexString) {
+		
+		byte[] b = new BigInteger(hexString,16).toByteArray();
+		
+		for (int i = 0; i < b.length; i++) {
+			
+			int current = b[i] & 0xff;
+			int shifted = ((current - thisShift) % byteRange);
+			if (shifted < 0) {
+				shifted += byteRange;
+				}
+			
+			b[i] = (byte) shifted;
+		}
+		
+		String outputString = new String(b, StandardCharsets.UTF_8);	
+		return(outputString);
+	}
+	
 
 }
-
-//
-//+ encrypt(String message): 
-//	   String
-//	+ decrypt(String message):
-//	   String 
-//	+ generateAesKey()
