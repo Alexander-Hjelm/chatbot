c11053b6a349cae8a89aa63fd1ab75cab53ee20a
Alexander Hjelm <admin@alexander-hjelm.com>
2017-03-05 11:47:06 +0100

    Auto-format ChatUI

--- src/ChatUI.java
+++ src/ChatUI.java
@@ -27,21 +27,17 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
-
-
-
-public class ChatUI extends JFrame{
+public class ChatUI extends JFrame {
 
 	private CommunicationsHandler communicationsHandler;
 
 	private JPanel panel;
 	private JTextPane messageArea;
 	private JScrollPane messageScrollPane;
-	private JLabel titleLabel; 
+	private JLabel titleLabel;
 	private JLabel otherNamesLabel;
 	private JTextField myMessagePane;
-	
-	
+
 	private JButton sendButton;
 	private JButton exitButton;
 	private JButton sendFileButton;
@@ -51,11 +47,9 @@ public class ChatUI extends JFrame{
 	private JMenuBar menuBar = new JMenuBar();
 	private JMenu menu = new JMenu("Menu");;
 	private JMenuItem newChatMenuItem;
-	
+
 	private MyData myData;
-	
-	
-	
+
 	public ChatUI(CommunicationsHandler communicationsHandlerIn, MyData myDataIn) {
 		super("Chat Window");
 		this.communicationsHandler = communicationsHandlerIn;
@@ -65,229 +59,216 @@ public class ChatUI extends JFrame{
 		buttonAction();
 		createAndShowGUI(panel);
 
-		//Build menu bar
+		// Build menu bar
 		menuBar.add(menu);
-		
+
 		newChatMenuItem = new JMenuItem("Start new chat");
 		newChatMenuItem.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	           	 IntroUI newIntroUI = new IntroUI();
-	         }
-	      });
-		
+			public void actionPerformed(ActionEvent e) {
+				IntroUI newIntroUI = new IntroUI();
+			}
+		});
+
 		menu.add(newChatMenuItem);
 		this.setJMenuBar(menuBar);
 	}
-	
-	
 
-	
-	
 	private void buttonAction() {
 		sendButton = new JButton("send");
-		exitButton = new JButton("exit");;
+		exitButton = new JButton("exit");
+		;
 		sendFileButton = new JButton("send file");
-		
-		
+
 		sendButton.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	           	 sendMessage();
-	         }
-	      });
-		
-		
-		
+			public void actionPerformed(ActionEvent e) {
+				sendMessage();
+			}
+		});
+
 		exitButton.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	            try {
+			public void actionPerformed(ActionEvent e) {
+				try {
 					communicationsHandler.exit();
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-	            
-	         }
-	      });
-		
+
+			}
+		});
+
 		sendFileButton.addActionListener(new ActionListener() {
-	         public void actionPerformed(ActionEvent e) {
-	        	 //Show a file chooser GUI
-	             int returnVal = fileChooser.showOpenDialog(panel);
+			public void actionPerformed(ActionEvent e) {
+				// Show a file chooser GUI
+				int returnVal = fileChooser.showOpenDialog(panel);
 
-	             if (returnVal == JFileChooser.APPROVE_OPTION) {
-	            	 File file = fileChooser.getSelectedFile();
-	            	 
-	            	 if (communicationsHandler instanceof Server) {
-	            		 FileUserSelectorUI fileUI = new FileUserSelectorUI(file, myMessagePane.getText(), communicationsHandler);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
 
-	            	 } else {
-	            		// This is a client, send file request to server.
-		                 communicationsHandler.sendFileRequest(file, myMessagePane.getText(), null);
-	            	 }
-	            	 
-	         		myMessagePane.setText("");
-	             }
-	         }
-	      });
-		
+					if (communicationsHandler instanceof Server) {
+						FileUserSelectorUI fileUI = new FileUserSelectorUI(file, myMessagePane.getText(),
+								communicationsHandler);
+
+					} else {
+						// This is a client, send file request to server.
+						communicationsHandler.sendFileRequest(file, myMessagePane.getText(), null);
+					}
+
+					myMessagePane.setText("");
+				}
+			}
+		});
+
 	}
 
 	private void createAndShowGUI(JPanel panel) {
-		
+
 		messageArea = new JTextPane();
 		messageArea.setEditable(false);
-		
+
 		messageScrollPane = new JScrollPane(messageArea);
-		titleLabel = new JLabel(myData.communicationType + ": " + myData.userName); 
-		otherNamesLabel  = new JLabel();
+		titleLabel = new JLabel(myData.communicationType + ": " + myData.userName);
+		otherNamesLabel = new JLabel();
 		myMessagePane = new JTextField();
 
-		
-		
-		//make myMessagePane listen to enters and send message if pressed. might better be defined elsewhere
+		// make myMessagePane listen to enters and send message if pressed.
+		// might better be defined elsewhere
 		myMessagePane.addKeyListener(new KeyListener() {
-			
-			public void keyTyped(KeyEvent e) {
-				
-			}
-			
-			public void keyReleased(KeyEvent e) {
-				
-			}
-			
-			public void keyPressed(KeyEvent e) {
-		        if (e.getKeyCode()==KeyEvent.VK_ENTER){
-		        	sendMessage();
-		        }
-		    
 
-		    }
+			public void keyTyped(KeyEvent e) {
+
+			}
+
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					sendMessage();
+				}
+
+			}
 		});
-		
+
 		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints(); 
-		
+		GridBagConstraints c = new GridBagConstraints();
+
 		// add stuff to panel then add panel to frame.
 		c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 0;
-        c.gridx = 1;
-        c.weightx = 0.5;
-        panel.add(titleLabel,c);
-		
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 2;
-        c.gridx = 0;
-        c.weightx = 1;
-        panel.add(myMessagePane, c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 2;
-        c.gridx = 1;
-        c.weightx = 1;
-        panel.add(sendButton,c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 3;
-        c.gridx = 1;
-        c.weightx = 1;
-        panel.add(sendFileButton,c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 3;
-        c.gridx = 0;
-        c.weightx = 1;
-        panel.add(progressBar,c);
-        
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridy = 0;
-        c.gridx = 0;
-        c.weightx = 1;
-        panel.add(exitButton,c);
-        
-        
-		
-        c.fill = GridBagConstraints.BOTH;
-        c.gridwidth = GridBagConstraints.REMAINDER;
-        c.gridx = 0;
-        c.gridy = 1;
-        c.weightx = 1;
-        c.weighty = 1;
-        panel.add(messageScrollPane,c);
-		
-		
-		
-	    //Create and set up the window.
-        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        
-        this.addWindowListener( new WindowAdapter()
-        {
-            public void windowClosing(WindowEvent e)
-            {
-            	
-            	try {
+		c.gridy = 0;
+		c.gridx = 1;
+		c.weightx = 0.5;
+		panel.add(titleLabel, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 2;
+		c.gridx = 0;
+		c.weightx = 1;
+		panel.add(myMessagePane, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 2;
+		c.gridx = 1;
+		c.weightx = 1;
+		panel.add(sendButton, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 3;
+		c.gridx = 1;
+		c.weightx = 1;
+		panel.add(sendFileButton, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 3;
+		c.gridx = 0;
+		c.weightx = 1;
+		panel.add(progressBar, c);
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridy = 0;
+		c.gridx = 0;
+		c.weightx = 1;
+		panel.add(exitButton, c);
+
+		c.fill = GridBagConstraints.BOTH;
+		c.gridwidth = GridBagConstraints.REMAINDER;
+		c.gridx = 0;
+		c.gridy = 1;
+		c.weightx = 1;
+		c.weighty = 1;
+		panel.add(messageScrollPane, c);
+
+		// Create and set up the window.
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+
+				try {
 					communicationsHandler.exit();
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-            	
-                JFrame frame = (JFrame)e.getSource();
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            }
-        });
-	  
-	    //Add contents to the window.
+
+				JFrame frame = (JFrame) e.getSource();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+
+		// Add contents to the window.
 		this.add(panel);
-	
-	    //Display the window.
+
+		// Display the window.
 		this.setSize(300, 300);
 		this.setVisible(true);
-		
-		
+
 	}
-	
+
 	public void showFileReceiverUI(String fileName, long fileSize, int userIndex) {
 		fileReceiverUI = new FileReceiverUI(communicationsHandler, fileName, fileSize, userIndex);
 	}
-	
+
 	private void sendMessage() {
-		// might want to build XML-message here, or it could be constructed in client/server with the string sent.
+		// might want to build XML-message here, or it could be constructed in
+		// client/server with the string sent.
 		String text = myMessagePane.getText();
 		Message msg = new Message(text, myData.userName, myData.color);
-		
+
 		communicationsHandler.send(msg);
-							  
-//		De-escape xml-specific characters
+
+		// De-escape xml-specific characters
 		XmlParser xmlParser = new XmlParser(myData);
 		String outText = xmlParser.deEscapeXMLChars(msg.text);
 		msg.text = outText;
-		
+
 		myMessagePane.setText("");
 		updateMessageArea(msg);
 
 	}
-	
-	public void updateMessageArea(Message msg) {
-		//Set text color
-        StyleContext sc = StyleContext.getDefaultStyleContext();
-        AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, msg.color);
 
-        int len = messageArea.getDocument().getLength();
-    	String appendStr = msg.sender + ": " + msg.text + "\n";
-        
-    	//Print text in MessageArea
-        try {
+	public void updateMessageArea(Message msg) {
+		// Set text color
+		StyleContext sc = StyleContext.getDefaultStyleContext();
+		AttributeSet aset = sc.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, msg.color);
+
+		int len = messageArea.getDocument().getLength();
+		String appendStr = msg.sender + ": " + msg.text + "\n";
+
+		// Print text in MessageArea
+		try {
 			messageArea.getDocument().insertString(len, appendStr, aset);
 		} catch (BadLocationException e) {
 			e.printStackTrace();
 		}
-        
+
 	}
 
-	public void setProgressBarFill (int i) {
+	public void setProgressBarFill(int i) {
 		progressBar.setValue(i);
 	}
-	
+
 	public void toggleSendFileButton() {
 		sendFileButton.setEnabled(!sendFileButton.isEnabled());
 	}
-	
+
 }
